5016c252f647b9bcac2924428985356a56e5deaa
lashasaata <lashasaata@gmail.com>
2024-02-07 06:39:33 -0800

    add mobile design and little changes

--- script.js
+++ script.js
@@ -14,45 +14,35 @@ range.addEventListener("input", (event) => {
   if (rangeValue == 1 && chackPoint == "closed") {
     viewAmount.innerText = "10K";
     costAmount.innerText = `$8.00`;
-    month_year.innerText = "/ month";
   } else if (rangeValue == 2 && chackPoint == "closed") {
     viewAmount.innerText = "50K";
     costAmount.innerText = `$12.00`;
-    month_year.innerText = "/ month";
   } else if (rangeValue == 3 && chackPoint == "closed") {
     viewAmount.innerText = "100K";
     costAmount.innerText = `$16.00`;
-    month_year.innerText = "/ month";
   } else if (rangeValue == 4 && chackPoint == "closed") {
     viewAmount.innerText = "500K";
     costAmount.innerText = `$24.00`;
-    month_year.innerText = "/ month";
   } else if (rangeValue == 5 && chackPoint == "closed") {
     viewAmount.innerText = "1M";
     costAmount.innerText = `$36.00`;
-    month_year.innerText = "/ month";
   }
 
   if (rangeValue == 1 && chackPoint == "opened") {
     viewAmount.innerText = "10K";
     costAmount.innerText = `$72.00`;
-    month_year.innerText = "/ year";
   } else if (rangeValue == 2 && chackPoint == "opened") {
     viewAmount.innerText = "50K";
     costAmount.innerText = `$108.00`;
-    month_year.innerText = "/ year";
   } else if (rangeValue == 3 && chackPoint == "opened") {
     viewAmount.innerText = "100K";
     costAmount.innerText = `$144.00`;
-    month_year.innerText = "/ year";
   } else if (rangeValue == 4 && chackPoint == "opened") {
     viewAmount.innerText = "500K";
     costAmount.innerText = `$216.00`;
-    month_year.innerText = "/ year";
   } else if (rangeValue == 5 && chackPoint == "opened") {
     viewAmount.innerText = "1M";
     costAmount.innerText = `$324.00`;
-    month_year.innerText = "/ year";
   }
   // sets progress div's width
   width = ((rangeValue - 1) / 4) * 100 + "%";
@@ -68,10 +58,12 @@ checkbox.addEventListener("change", () => {
     price = (price * 12 * 75) / 100;
     costAmount.innerText = `$${price.toFixed(2)}`;
     chackPoint = "opened";
+    month_year.innerText = "/ year";
   } else if (chackPoint == "opened") {
     let price = parseFloat(costAmount.innerText.replace(/[^\d.-]/g, ""));
     price = (price * 100) / 75 / 12;
     costAmount.innerText = `$${price.toFixed(2)}`;
     chackPoint = "closed";
+    month_year.innerText = "/ month";
   }
 });
